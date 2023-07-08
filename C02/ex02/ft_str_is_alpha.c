8158f008851d0f78a37ffdbe8a699af532fd3507
loris <loris.gaume@42.ch>
2023-07-08 05:50:44 +0200

    adding ft_str_is_alpha.c

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,14 @@
+
+int ft_str_is_alpha(char *str)
+{
+    int x;
+
+    x = 0;
+    while (str[x])
+    {
+        if ((str[x] < 'a' || str[x] > 'z') && (str[x] < 'A' || str[x] > 'Z'))
+            return(0);
+        x++;
+    }
+    return (1);
+}
